81abac79d670c0a97496a4e0db0bdd8f672112c4
Joshua <jabarcap.2004@gmail.com>
2026-01-12 21:23:09 -0500

    Activando ruta dashboard

--- MCombat/urls.py
+++ MCombat/urls.py
@@ -3,21 +3,21 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from asistencia import views
+from asistencia import views  # Importamos tus vistas (incluyendo el dashboard)
 
 urlpatterns = [
     # --- 1. RUTA NUEVA DEL DASHBOARD (¡Debe ir PRIMERO!) ---
-    # Si no la pones antes de 'admin/', no funcionará.
-    path('admin/dashboard/', views.dashboard_view, name='admin_dashboard'),
+    # Al ponerla aquí, interceptamos la url antes de que entre al admin normal
+    path('admin/dashboard/', views.dashboard_view, name='dashboard'),
 
     # 2. Panel de Administrador (Va después)
     path('admin/', admin.site.urls),
 
-    # 3. Login y Logout para Entrenadores
+    # 3. Login y Logout para Staff
     path('login/', auth_views.LoginView.as_view(template_name='login_asistencia.html'), name='login_asistencia'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login_asistencia'), name='logout'),
 
-    # 4. Pantalla de Registro de Asistencia (La Home)
+    # 4. Pantalla de Registro de Asistencia (La Home / Scanner)
     path('', views.registro_asistencia, name='registro_asistencia'),
 
     # 5. Exportar Excel
@@ -30,6 +30,6 @@ urlpatterns = [
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
-# --- BLOQUE MÁGICO PARA VER LAS FOTOS ---
+# --- BLOQUE MÁGICO PARA VER LAS FOTOS (Modo Debug) ---
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
