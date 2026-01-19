0c277b61e27b9a72d112d5d5e8634725c027c133
Junior Programmer <jabarcap.2004@gmail.com>
2026-01-18 22:03:57 -0500

    Regresando al puerto 2525 para evitar bloqueo

--- MCombat/settings.py
+++ MCombat/settings.py
@@ -192,7 +192,7 @@ JAZZMIN_UI_TWEAKS = {
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 30
